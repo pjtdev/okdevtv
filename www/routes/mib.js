a65b22bc197a3bc48418c6cd1c071f32daba0beb
kenu <kenu.heo@gmail.com>
2016-06-03 11:28:59 +0900

    GA script debug

--- www/routes/mib.js
+++ www/routes/mib.js
@@ -31,21 +31,21 @@ function setBody(data, path) {
         <h1 id="logo"><a href="/"><img \
         src="/images/logo.png" style="width: 36px; margin-right: 6px; margin-bottom: -6px;">OKdevTV</a></h1>\
     </header>' + html + "\
-    <script>\
-        (function (i, s, o, g, r, a, m) {\
-            i['GoogleAnalyticsObject'] = r;\
-            i[r] = i[r] || function () {\
-                (i[r].q = i[r].q || []).push(arguments)\
-            }, i[r].l = 1 * new Date();\
-            a = s.createElement(o),\
-                m = s.getElementsByTagName(o)[0];\
-            a.async = 1;\
-            a.src = g;\
-            m.parentNode.insertBefore(a, m\
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');\
-\
-        ga('create', 'UA-49100336-1', 'auto');\
-        ga('send', 'pageview');</script>\
+    <script>\n\
+        (function (i, s, o, g, r, a, m) {\n\
+            i['GoogleAnalyticsObject'] = r;\n\
+            i[r] = i[r] || function () {\n\
+                (i[r].q = i[r].q || []).push(arguments)\n\
+            }, i[r].l = 1 * new Date();\n\
+            a = s.createElement(o),\n\
+                m = s.getElementsByTagName(o)[0];\n\
+            a.async = 1;\n\
+            a.src = g;\n\
+            m.parentNode.insertBefore(a, m)\n\
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');\n\
+\n\
+        ga('create', 'UA-49100336-1', 'auto');\n\
+        ga('send', 'pageview');</script>\n\
 </body></html>";
     return html;
 }
